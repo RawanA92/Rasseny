0550e404e8bd44981d81d099d3bd3beb770dec9c
ShahdMahmoud19 <123309438+ShahdMahmoud19@users.noreply.github.com>
2024-05-11 10:15:28 +0300

    style forgetpassword

--- RassenyApp/app/screens/ForgetPassword.jsx
+++ RassenyApp/app/screens/ForgetPassword.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, Text, StyleSheet } from "react-native";
+import { View, TextInput, Button, Text, StyleSheet, Pressable } from "react-native";
 import { forgetPassword } from "../../firebase/auth"; 
-
 const ForgetPassword = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
@@ -18,39 +17,63 @@ const ForgetPassword = () => {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        style={styles.input}
-      />
-      <Button title="Reset Password" onPress={handleForgetPassword} />
-      {error ? <Text style={styles.error}>{error}</Text> : null}
-      {successMessage ? (
-        <Text style={styles.success}>{successMessage}</Text>
-      ) : null}
-    </View>
+    <TextInput
+      placeholder="Email"
+      value={email}
+      onChangeText={setEmail}
+      style={styles.input}
+    />
+    <Pressable
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? 'blue' : 'purple', 
+          padding: 10,
+          borderRadius: 50,
+          alignItems: 'center',
+          marginLeft:10,
+          marginRight:10,
+        },
+        styles.button
+      ]}
+      onPress={handleForgetPassword}
+    >
+      <Text style={{ color: 'white', textAlign: 'center' }}>Reset Password</Text>
+    </Pressable>
+    {error ? <Text style={styles.error}>{error}</Text> : null}
+    {successMessage ? (
+      <Text style={styles.success}>{successMessage}</Text>
+    ) : null}
+  </View>
+  
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center",
-    margin: 20,
+    margin: 25,
+    backgroundColor:"pink",
+    borderRadius:50
   },
   input: {
     borderWidth: 1,
-    borderColor: "gray",
     padding: 10,
     marginBottom: 10,
-    width: "100%",
+    marginRight:10,
+    marginLeft:10,
+    backgroundColor:"white",
+    borderColor:"pink",
+    borderRadius:50
   },
   error: {
     color: "red",
     marginTop: 10,
+    fontSize:16,
+    marginLeft:15,
   },
+  
   success: {
     color: "green",
     marginTop: 10,
@@ -58,4 +81,3 @@ const styles = StyleSheet.create({
 });
 
 export default ForgetPassword;
-
